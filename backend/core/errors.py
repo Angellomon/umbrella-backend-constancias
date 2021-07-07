0467cacffd3adc00c7efa0c8067bd3c215b1488f
Angellomon <angelmtzdiaz@gmail.com>
2021-07-07 00:00:35 -0500

    feat(errors): Error Classes

---
+++ backend/core/errors.py
@@ -0,0 +1,10 @@
+from fastapi.exceptions import HTTPException
+from fastapi import status
+
+
+class EventoNotFound(Exception):
+    def __init__(self, clave_evento: str) -> None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"evento no encontrado (clave={clave_evento})",
+        )
