c4f9ed1a95708f58f1c359c13c0ecd9447821e22
Angellomon <angelmtzdiaz@gmail.com>
2022-07-13 18:29:28 -0500

    chore: inclusión de nuevos endpoints

--- backend/api/api.py
+++ backend/api/api.py
@@ -5,6 +5,7 @@ from .endpoints.empresas import router as empresas_router
 from .endpoints.eventos import router as eventos_router
 from .endpoints.oauth import router as oauth_router
 from .endpoints.users import router as usuarios_router
+from .endpoints.pdf import router as pdf_router
 
 router = APIRouter()
 
@@ -14,3 +15,4 @@ router.include_router(usuarios_router, prefix="/users")
 router.include_router(oauth_router, prefix="/oauth")
 router.include_router(eventos_router, prefix="/eventos")
 router.include_router(empresas_router, prefix="/empresas")
+router.include_router(pdf_router, prefix="/pdf")
